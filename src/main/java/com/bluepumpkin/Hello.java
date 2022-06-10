28913caa86b9f85502cf545b1ba815e15ffc1711
yuhaodai <93068257+B1uePumpkin@users.noreply.github.com>
2022-06-10 15:00:27 +0800

    Person

--- src/main/java/com/bluepumpkin/Hello.java
+++ src/main/java/com/bluepumpkin/Hello.java
@@ -3,5 +3,7 @@ package com.bluepumpkin;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Person p=new Person();
+        p.greeting();
     }
 }
---
+++ src/main/java/com/bluepumpkin/Person.java
@@ -0,0 +1,7 @@
+package com.bluepumpkin;
+
+public class Person {
+    public void greeting(){
+        System.out.println("Hello!");
+    }
+}
